9841c7b2fae9ba644a6bab92bfc9eff0e348bde2
samruddhi4 <samruddhiwaykos@gmail.com>
2019-11-14 15:02:02 +0530

    java

---
+++ Axis.java
@@ -0,0 +1,81 @@
+package BankAccount;
+
+public class Axis {
+	public int ID;
+	public String Name;
+	public int MobileNumber;
+	public int AccountNumber;
+	public int AdharNumber;
+	public int PanNumber;
+	public char UserName;
+	public String Password;
+		public int getID()
+	{
+		return ID;
+		}
+		public String getName()
+		{
+			return Name;
+			}
+		public int getMobileNumber()
+		{
+			return MobileNumber;
+			}
+		public int getAccountNumber()
+		{
+			return AccountNumber;
+			}
+		public int getAdharNumber()
+		{
+			return AdharNumber;
+			}
+		public int getPanNumber()
+		{
+			return PanNumber;
+			}
+		public char getUserName()
+		{
+			return UserName;
+			}
+		public String getPassword()
+		{
+			return Password;
+			}
+		public void setID()
+		{
+			this.ID= ID;
+		}
+		public void setName()
+		{
+			this.Name= Name;
+			}
+		public void setMobileNumber()
+		{
+			this.MobileNumber=MobileNumber;
+				}
+		public void setAccountNumber()
+		{
+			this.AccountNumber= AccountNumber;
+			
+		}
+		public void setAdharNumber()
+		{
+			this.AdharNumber= AdharNumber;
+			
+		}
+		public void setPanNumber()
+		{
+			this.PanNumber= PanNumber;
+			
+		}
+		public void setUserName()
+		{
+			this.UserName= UserName;
+			
+		}
+		public void setPassword()
+		{
+			this.Password= Password;
+			
+		}
+}
